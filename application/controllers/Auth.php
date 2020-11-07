ce9d4df9591f738877afc4b25ef0232e83fefd9e
Firofa <rizifaarsien@gmail.com>
2020-11-07 23:48:05 +0700

    Menambahkan fitur register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,17 +3,61 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+	public function __construct() 
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
-		$this->load->view('templates/auth_header');
+		$data['title'] = 'Login | SIPERMA';
+		$this->load->view('templates/auth_header',$data);
 		$this->load->view('auth/login_view');
 		$this->load->view('templates/auth_footer');
 	}
 
 	public function registration()
 	{
-		$this->load->view('templates/auth_header');
-		$this->load->view('auth/registration_view');
-		$this->load->view('templates/auth_footer');
+		$this->form_validation->set_rules("name","Name","required|trim");
+		$this->form_validation->set_rules("nip","NIP","required|trim|numeric");
+		$this->form_validation->set_rules("jabatan","Jabatan","required|trim");
+		$this->form_validation->set_rules("pangkat","Pangkat","required|trim");
+		$this->form_validation->set_rules("tahun","Tahun","required|trim|numeric");
+		$this->form_validation->set_rules("username","Username","required|trim|is_unique[users.username]");
+		$this->form_validation->set_message('is_unique','The Username already used.');
+		$this->form_validation->set_rules("password","Password","required|trim|min_length[8]|matches[password_confirmation]");
+		$this->form_validation->set_message('matches','Password not match');
+		$this->form_validation->set_message('min_length','Password too short');
+		$this->form_validation->set_rules("password_confirmation","Password Confirmaation","required|trim|matches[password]");
+		$this->form_validation->set_rules("level_akses_id","Level Akses","required|trim");
+		$this->form_validation->set_rules("work_unit_id","Unit Kerja","required|trim");
+		$this->form_validation->set_rules("ruangan_id","Ruangan","required|trim");
+
+
+		if($this->form_validation->run() == false) {
+			$data['title'] = 'Registration | SIPERMA';
+			$this->load->view('templates/auth_header',$data);
+			$this->load->view('auth/registration_view');
+			$this->load->view('templates/auth_footer');
+		} else {
+			$data = [
+				'name' => htmlspecialchars($this->input->post('name'),true),
+				'nip' => htmlspecialchars($this->input->post('nip'),true),
+				'jabatan' => htmlspecialchars($this->input->post('jabatan'),true),
+				'pangkat' => htmlspecialchars($this->input->post('pangkat'),true),
+				'tahun' => htmlspecialchars($this->input->post('tahun'),true),
+				'username' => htmlspecialchars($this->input->post('username'),true),
+				'password' => password_hash($this->input->post('password'),PASSWORD_DEFAULT),
+				'level_access_id' => htmlspecialchars($this->input->post('level_access_id'),true),
+				'work_unit_id' => htmlspecialchars($this->input->post('work_unit_id'),true),
+				'ruangan_id' => htmlspecialchars($this->input->post('ruangan_id'),true),
+				'is_active' => 1,
+				'created_at' => time()
+			];
+			$this->db->insert('users',$data);
+			$this->session->set_flashdata("message","<div class='alert alert-success' role='alert'>Akun berhasil terdaftar.</div>");
+			redirect('auth/registration');
+		}
 	}
 }
--- application/views/auth/registration_view.php
+++ application/views/auth/registration_view.php
@@ -8,36 +8,44 @@
             <div class="p-5">
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Halaman Registrasi</h1>
+                <?= $this->session->flashdata('message'); ?>
               </div>
-              <form class="user">
+              <form class="user" method="POST" action="<?= base_url('auth/registration'); ?>">
                 <div class="form-group row">
                     <label>Nama Pengguna:</label>
-                    <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Masukan Nama Pengguna...">
+                    <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Masukan Nama Pengguna..." value="<?= set_value('name'); ?>">
+                    <?= form_error('name','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                     <label>NIP:</label>
-                    <input type="text" class="form-control form-control-user" id="nip" name="nip" placeholder="Masukan Nomor Induk Pegawai...">
+                    <input type="text" class="form-control form-control-user" id="nip" name="nip" placeholder="Masukan Nomor Induk Pegawai..." value="<?= set_value('nip'); ?>">
+                    <?= form_error('nip','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                     <label>Jabatan:</label>
-                    <input type="text" class="form-control form-control-user" id="jabatan" name="jabatan" placeholder="Masukan Jabatan Pegawai...">
+                    <input type="text" class="form-control form-control-user" id="jabatan" name="jabatan" placeholder="Masukan Jabatan Pegawai..." value="<?= set_value('jabatan'); ?>">
+                    <?= form_error('jabatan','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                     <label>Pangkat:</label>
-                    <input type="text" class="form-control form-control-user" id="pangkat" name="pangkat" placeholder="Masukan Pangkat Pegawai...">
+                    <input type="text" class="form-control form-control-user" id="pangkat" name="pangkat" placeholder="Masukan Pangkat Pegawai..." value="<?= set_value('pangkat'); ?>">
+                    <?= form_error('pangkat','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                     <label>Tahun:</label>
-                    <input type="number" class="form-control form-control-user" id="pangkat" name="pangkat" placeholder="Masukan Tahun Masuk Pegawai...">
+                    <input type="number" class="form-control form-control-user" id="tahun" name="tahun" placeholder="Masukan Tahun Masuk Pegawai..." value="<?= set_value('tahun'); ?>">
+                    <?= form_error('tahun','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                     <label>Username:</label>
-                    <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Masukan Username...">
+                    <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Masukan Username..." value="<?= set_value('username'); ?>">
+                    <?= form_error('username','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <label>Password:</label>
                     <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                    <?= form_error('password','<small class="text-danger pl-3">','</small>'); ?> 
                   </div>
                   <div class="col-sm-6">
                     <label>Konfirmasi Password:</label>
@@ -53,6 +61,7 @@
                     <option>4</option>
                     <option>5</option>
                   </select>
+                    <?= form_error('level_akses_id','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                   <label for="work_unit_id">Unit Kerja:</label>
@@ -63,6 +72,7 @@
                     <option>4</option>
                     <option>5</option>
                   </select>
+                    <?= form_error('work_unit_id','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <div class="form-group row">
                   <label for="ruangan_id">Ruangan:</label>
@@ -73,6 +83,7 @@
                     <option>4</option>
                     <option>5</option>
                   </select>
+                    <?= form_error('ruangan_id','<small class="text-danger pl-3">','</small>'); ?> 
                 </div>
                 <button type="submit" class="btn btn-primary btn-user btn-block">
                   Register Account
